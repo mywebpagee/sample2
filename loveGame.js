2bc23b8b12b5042ae8fbb4c9333f1015399d34a4
mywebpagee <92183561+mywebpagee@users.noreply.github.com>
2021-10-08 23:39:46 +0100

    Add files via upload

---
+++ loveGame.js
@@ -0,0 +1,22 @@
+prompt ("what is your name");
+    prompt ("What is your crush name");
+    var love = Math.floor(Math.random()*100)+1;
+    
+    if (love<=30){
+        alert("pls stay single");
+    }
+
+    else if (love >=31 && love <=50){
+        alert("Your love Grade " +love+" You are both manageable")
+    }
+
+    else if (love >=51 && love <=80){
+    alert("Your love Grade "+ love+" Your love is real")
+    }
+
+    else{
+        alert("Your love Grade " +love+" You were made for each other. Go and get married!")
+    }
+
+   
+    
